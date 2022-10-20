e40327152440689d7537f78c7fcda82d5dd247d1
imajs7 <anukhg@gmail.com>
2022-10-20 15:22:35 +0530

    netlify form requirement fulfilled

--- src/componenets/commons/ContactForm/ContactForm.tsx
+++ src/componenets/commons/ContactForm/ContactForm.tsx
@@ -6,7 +6,9 @@ const ContactForm = () => {
 
             <h2 className='form-title' id="get-a-quote">Get a Quote</h2>
             
-            <form name="contact" action="POST" method="POST">
+            <form name="contact" action="POST" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+
+                <input type="hidden" name="bot-field" />
 
                 <div className="fullName">
                     <input 
@@ -44,6 +46,8 @@ const ContactForm = () => {
                         required
                     ></textarea>
                 </div>
+
+                <div data-netlify-recaptcha="true"></div>
 
                 <div className="buttons">
                     <button type="reset">Reset</button>
